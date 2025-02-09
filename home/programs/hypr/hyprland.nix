83201e66a90f77dcc444af5f47955c2465ee8b29
sudanbhurtel <sudanbhurtel@gmail.com>
2025-02-09 14:45:33 +0900

    just a little change

--- home/programs/hypr/hyprland.nix
+++ home/programs/hypr/hyprland.nix
@@ -168,7 +168,7 @@
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-	",XF86MonBrightnessUp, exec, brightnessctl s 5%"
+	",XF86MonBrightnessUp, exec, brightnessctl s 5%+"
         ",XF86MonBrightnessDown, exec, brightnessctl s 5%-"
       ];
 
